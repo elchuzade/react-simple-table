597a5837158dc3ff09e2263a45b200b6fb493311
Kamran Elchuzade <elchuzade@gmail.com>
2020-10-17 21:03:23 +0300

    Updates

---
+++ precache-manifest.38957ab16990ec652335acce90df093c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4da2e2614dcdb2e53931fa139a90614",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb2a0208ec8152b9336c",
+    "url": "./static/css/main.8e601755.chunk.css"
+  },
+  {
+    "revision": "d05aff098aa37a48070e",
+    "url": "./static/js/2.b19235b6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b19235b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2a0208ec8152b9336c",
+    "url": "./static/js/main.63e7a88e.chunk.js"
+  },
+  {
+    "revision": "5bc714a3fd40c8665634",
+    "url": "./static/js/runtime-main.c5fcf3ac.js"
+  }
+]);
